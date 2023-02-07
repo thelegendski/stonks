14c5bfbe743b66964ad3c654cfb625e4661adfe3
ski <108815730+thelegendski@users.noreply.github.com>
2023-02-07 13:07:37 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 //add fetch call here
 const token = "cfgk7khr01qlga2ufnb0cfgk7khr01qlga2ufnbg", symbol = "HMC"
 
-println(1)
-println(fetch)
-println(fetch(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`))
+async function getData (URL) {
+  const response = await fetch(URL)
+  const data = await response.json()
+  println(JSON.stringify(data), response)
+}
+
+getData(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`)
