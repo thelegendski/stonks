873e489cfc0616be601298a79b84177a6c3bbeda
ski <108815730+thelegendski@users.noreply.github.com>
2023-02-07 08:23:27 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 //add fetch call here
-const token = cfgk7khr01qlga2ufnb0cfgk7khr01qlga2ufnbg
+const token = "cfgk7khr01qlga2ufnb0cfgk7khr01qlga2ufnbg"
 
 fetch(`https://finnhub.io/api/v1/quote?symbol=HMC&token=${token}`).then(response => println(JSON.stringify(response), typeof response))
