907ea10641054d15a5bf831355923b73455d99f1
ski <108815730+thelegendski@users.noreply.github.com>
2023-02-08 08:15:34 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-//add fetch call here
-const token = "cfgk7khr01qlga2ufnb0cfgk7khr01qlga2ufnbg", symbol = "HMC"
+//for my sanity
+Object.prototype.toString = function() {
+  return JSON.stringify(this)
+}
 
+//as simple as you can get
 async function getData (URL) {
   const response = await fetch(URL)
   const data = await response.json()
-  println(JSON.stringify(data), response)
+  return data
 }
 
-getData(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`)
+//function that will end up runnin' the whole program
+async function getStockData (symbol) {
+  //retrieve data here
+  const token = "cfgk7khr01qlga2ufnb0cfgk7khr01qlga2ufnbg"
+  const data = await getData(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`)
+  
+  //display data here
+  println(data)
+}
+
+getStockData("AAPL")
